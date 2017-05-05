fc13657982e515e59c95e40ab882d99aa8985324
Turan Sadri <turan@motleyagency.com>
2017-05-05 19:33:39 +0300

    Index updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { createStore } from 'redux';
 
-const apiKey = "AIzaSyCV31G_XGjKMer9YBaviREhz501w0EyOYs";
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
